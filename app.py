7f5c8b8b8aa7f42026198c5ebad879fe214f8343
jipuku <noah.shah3@gmail.com>
2021-11-14 15:33:25 -0800

    fixed bugs

--- app.py
+++ app.py
@@ -25,14 +25,16 @@ class Data:
             '1yr': d.get1yrData,
             'all': d.getAllData
         }
+
         threads = [th.Thread(target=self._call_back, args=(func, key)) for key, func in intervals.items()]
         for thread in threads: thread.start()
         for thread in threads: thread.join()
+        
         _json = {
             'ticker': ticker,
             'intervals': _data
         }
-        
+
         return _json
 
     def _call_back(self, func, key):
@@ -47,9 +49,6 @@ def ticker():
     returnData = {
         'historical':list(dataframe['Adj Close'])
     }
-    # analytics = Analytics(stock_data)
-    # predicted = analytics.predict()
-    # log = analytics.logs()
 
     return jsonify(returnData)
 
