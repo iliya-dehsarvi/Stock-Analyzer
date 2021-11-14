c8632e5fe1597ecdeb526557bf206a90feaa1d8e
mfish33 <maxmfishernj@gmail.com>
2021-11-14 14:01:27 -0800

    fixed api to work with frontend

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from StockData import StockData
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 class Data:
     def __init__(self, json):
@@ -11,14 +13,16 @@ class Data:
         return jsonify(self.json)
 
 
-@app.route("/", methods=["POST"])
+@app.route("/one", methods=["POST"])
 def ticker():
     # Send back 1MIN chart
     ticker = request.json["ticker"]
-    dataframe = StockData("AAPL").handler()
+    dataframe = StockData(ticker).handler()
+    returnData = {
+        'historical':list(dataframe['Adj Close'])
+    }
     # analytics = Analytics(stock_data)
     # predicted = analytics.predict()
     # log = analytics.logs()
 
-    return dataframe.to_json()
-
+    return jsonify(returnData)
