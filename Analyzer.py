75961b726676e496ce3933452bbba16324df3212
iliya52 <Dehsarvi.iliya@gmail.com>
2021-11-14 11:44:50 -0800

    Update Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -3,26 +3,29 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVR
+import threading as th
 import json
+import talib
 
 import Data, API
 
-class Analytics:
-    def __init__(self):
-        self.data = None
-        self.predicted_data = None
-        self.logs = None
-
 class Analyzer:
+    class Analytics:
+        def __init__(self):
+            self.data = None
+            self.predicted_data = None
+            self.analytics_note = None
+            self.logs = None
+
     def __init__(self, data):
         self.data = data
-        self.open = data['Open']
-        self.high = data['High']
-        self.low = data['Low']
-        self.close = data['Close']
-        self.adj_close = data['Adj Close']
-        self.volume = data['Volume']
-        self.graph(self.open, self.close, self.volume) 
+        self.open = list(data['Open'])
+        self.high = list(data['High'])
+        self.low = list(data['Low'])
+        self.close = list(data['Close'])
+        self.adj_close = list(data['Adj Close'])
+        self.volume = list(data['Volume'])
+        self.graph(self.adj_close, self.create_pridicted_data(self.adj_close), self.volume) 
 
     def graph(self, *args):
         number_of_graphs = len(args)
@@ -31,8 +34,50 @@ class Analyzer:
         fig.tight_layout()
         plt.show()
 
+    def pattern_recignition(self): pass
+
+    def create_pridicted_data(self, data):
+        chunked_data, chunk_length = self._data_in_chunks(data)
+        predictions = [self._predict(self._join(chunked_data[:i]), self._join(chunked_data[i+1:]), chunk_length) for i in range(len(chunked_data))]
+        prediction = []
+        for _prediction in predictions: prediction += _prediction
+        return prediction
+
+    def _predict(self, first_end, last_end, prediction_length):
+        if first_end:
+            first_end_indecies = [[index] for index in range(len(first_end))]
+            first_end_regression = SVR()
+            first_end_regression.fit(first_end_indecies, first_end)
+            first_end_prediction = [first_end_regression.predict([[index+len(first_end)]])[0] for index in range(prediction_length)]    
+        else:
+            first_end_prediction = []
+        
+        if last_end:  
+            last_end = last_end[::-1]
+            last_end_indecies = [[index] for index in range(len(last_end))]
+            last_end_regression = SVR()
+            last_end_regression.fit(last_end_indecies, last_end)
+            last_end_prediction = [last_end_regression.predict([[index+len(last_end)]])[0] for index in range(prediction_length)]
+        else:
+            last_end_prediction = []
+
+        prediction = [(first+last)/2 for first, last in zip(first_end_prediction, last_end_prediction[::-1])]         
+        return prediction
+
+    def _data_in_chunks(self, data, interval_rate=0.01):
+        interval = int(len(data)*interval_rate)
+        chunks = []
+        pre = 0
+        for curr in range(interval, len(data), interval):
+            chunks.append(data[pre: curr])
+            pre = curr
+        return chunks, interval
+
+    def _join(self, data):
+        values = []
+        for value in data: values += value
+        return values
+
 if __name__ == '__main__':
     data = yf.download('AAPL', period='5d', interval='1m')
-    with open('/Users/iliyadehsarvi/Documents/GitHub/Stock-Analyzer/api.json', 'w') as api:
-        api.write(data.to_json())
-    # App = Analyzer(data)
+    App = Analyzer(data)
