d6715422c384af071a5019d912c68b940b0be345
jipuku <noah.shah3@gmail.com>
2021-11-14 14:21:26 -0800

    added predicted data endpoint

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask, jsonify, request
 from StockData import StockData
 from Analyzer import Analyzer
+import threading as th
 
 app = Flask(__name__)
 
 class Data:
-    def __init__(self, json):
-        self.json = json
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self._data = {}
+        self.a = Analyzer()
 
-    def convertToJSON(self):
-        return jsonify(self.json)
+    def data(self):
+        self._data = StockData(ticker)
+
+        intervals = {
+            '1d': d.get1dData,
+            '5d': d.get5dData,
+            '1mo': d.get1moData,
+            '3mo': d.get3moData,
+            '6mo': d.get6moData,
+            '1yr': d.get1yrData,
+            'all': d.getAllData
+        }
+        threads = [th.Thread(target=call_back, args=(func, key)) for key, func in intervals.items()]
+        for thread in threads: thread.start()
+        for thread in threads: thread.join()
+        _json = {
+            'ticker': ticker,
+            'intervals': _data
+        }
+
+    def _call_back(self, func, key):
+        self._data[key] = self.a.get(func())
 
 
 @app.route("/one", methods=["POST"])
@@ -26,9 +49,8 @@ def ticker():
 
     return jsonify(returnData)
 
-
-# import Threading as th
-
-# threads = [th.Thread(target=call_back, args=(1,)]
-# for thread in threads: thread.start()
-# for thread in threads: thread.join()
+@app.route("/two", methods=["POST"])
+def all_data():
+    ticker = request.json["ticker"]
+    newData = Data(ticker).data()
+    return newData
