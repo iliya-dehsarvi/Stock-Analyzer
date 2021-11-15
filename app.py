e02745bac4a8a317de051d42f182e2ffcb45d3e2
mfish33 <maxmfishernj@gmail.com>
2021-11-14 16:38:11 -0800

    updated routes

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ class Data:
         self._data = StockData(ticker)
 
         intervals = {
-            '1d': d.get1dData,
-            '5d': d.get5dData,
-            '1mo': d.get1moData,
-            '3mo': d.get3moData,
-            '6mo': d.get6moData,
-            '1yr': d.get1yrData,
-            'all': d.getAllData
+            '1D': self._data.get1dData,
+            '5D': self._data.get5dData,
+            '1mo': self._data.get1moData,
+            '3mo': self._data.get3moData,
+            '6mo': self._data.get6moData,
+            '1yr': self._data.get1YrData,
+            'all': self._data.getAllData
         }
         threads = [th.Thread(target=self._call_back, args=(func, key)) for key, func in intervals.items()]
         for thread in threads: thread.start()
@@ -44,8 +44,13 @@ def ticker():
     # Send back 1MIN chart
     ticker = request.json["ticker"]
     dataframe = StockData(ticker).handler()
+    analytics = Analyzer()
+    predicted = analytics.get(dataframe['Adj Close'])
+
     returnData = {
-        'historical':list(dataframe['Adj Close'])
+        'historical':list(dataframe['Adj Close']),
+        'predicted':predicted
+
     }
     # analytics = Analytics(stock_data)
     # predicted = analytics.predict()
@@ -53,7 +58,7 @@ def ticker():
 
     return jsonify(returnData)
 
-@app.route("/two", methods=["POST"])
+@app.route("/all", methods=["POST"])
 def all_data():
     ticker = request.json["ticker"]
     newData = Data(ticker).data()
